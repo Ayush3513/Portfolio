08fc22582dc0a13d3f4adf5f512a5b18e6f3b8cb
Ayush3513 <ayushchavda2020@gmail.com>
2024-09-17 00:46:16 +0530

    nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,10 +13,15 @@ const Navbar = () => {
     <header className='text-white absolute top-0 left-0 right-0 bg-black/90 z-50'>
         <div className="max-w-7xl py-5 flex items-center justify-between mx-auto c-space">
             <a href="/" className='text-neutral-400 text-xl font-bold hover:text-white transition-all'>Ayush</a>
-            <button onClick={toggelHandler} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden' aria-label='Toggle Menu'><img className='h-6 w-6' src={Open ? "public/assets/close.svg" : "public/assets/menu.svg" } alt="toggle" /></button>
+            <label  className="flex flex-col gap-2 w-8">
+      <input onClick={toggelHandler} className="peer hidden" type="checkbox" />
+      <div className="rounded-2xl h-[3px] w-1/2 bg-white duration-500 peer-checked:rotate-[225deg] origin-right peer-checked:-translate-x-[12px] peer-checked:-translate-y-[1px]" />
+      <div className="rounded-2xl h-[3px] w-full bg-white duration-500 peer-checked:-rotate-45" />
+      <div className="rounded-2xl h-[3px] w-1/2 bg-white duration-500 place-self-end peer-checked:rotate-[225deg] origin-left peer-checked:translate-x-[12px] peer-checked:translate-y-[1px]" />
+    </label>
             <nav className='sm:flex hidden'><NavItems /></nav>
         </div>
-        <div className={`nav-sidebar ${Open ? "max-h-screen" : "max-h-0"}`}>
+        <div className={`nav-sidebar ${Open ? "max-h-screen" : "max-h-0"} `}>
           <nav className='p-5'>
             <NavItems />
           </nav>
--- src/Constants/index.js
+++ src/Constants/index.js
@@ -58,7 +58,7 @@ export const navLinks = [
   
   export const myProjects = [
     {
-      title: 'Podcastr - AI Podcast Platform',
+      title: 'CineStream - Movie Review Platform',
       desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
       subdesc:
         'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
